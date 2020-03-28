c9829101a201e8082d6798bc78d821235000db0d
vladyslavhorbachenko <vladgorbachenko1234@gmail.com>
2020-03-27 22:44:10 -0700

    done mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Form from './components/Form';
+import Home from './components/Home';
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
+function App() {
+    return (
+        <Router>
+            <div className="container">
+            <nav className="navbar">
+                <Link to="/">
+                    <button name = 'homebutton'>Home</button>
+                </Link>
+                <br/>
+                <Link to="/form">
+                    <button name = 'orderbutton'>Order</button>
+                </Link>
+            </nav>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/form" component={Form} />
+                </Switch>
+            </div>
+            </div>
+        </Router>
+    );
+}
+
 export default App;
