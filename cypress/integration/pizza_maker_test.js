3981564a4cb9b9be386c5c523acd8b8168f70fc5
vladyslavhorbachenko <vladgorbachenko1234@gmail.com>
2020-03-28 13:47:29 -0700

    all done

--- cypress/integration/pizza_maker_test.js
+++ cypress/integration/pizza_maker_test.js
@@ -1,15 +1,17 @@
 describe("Testing the pizza routes and form", function() {
     beforeEach(function() {
-        cy.visit("http://localhost:3003/");
+        cy.visit("http://localhost:3004/");
+
     });
+
     it("Add test to inputs and submit form", function() {
         cy
             .get('button[name="orderbutton"]')
             .click()
         cy
             .get('input[name="name"]')
-            .type("Jack")
-            .should("have.value", "Jack")
+            .type("Vlad")
+            .should("have.value", "Vlad")
         cy
             .get('textarea[name="specInstr"]')
             .type("some text")
@@ -28,6 +30,30 @@ describe("Testing the pizza routes and form", function() {
     });
 
     it("No toppings no instructions", function() {
+        cy
+            .get('button[name="orderbutton"]')
+            .click()
+        cy
+            .get('button[name="homebutton"]')
+            .click()
+        cy
+            .get('button[name="orderbutton"]')
+            .click()
+        cy
+            .get('input[name="name"]')
+            .type("Timmy")
+            .should("have.value", "Timmy")
+        cy
+            .get('select[name="size"]')
+            .select('Sm')
+            .should("have.value", "Sm")
+        cy
+            .get('button[name="submit"]')
+            .click()
+
+    });
+
+    it("Test that you can send the form", function() {
         cy
             .get('button[name="orderbutton"]')
             .click()
